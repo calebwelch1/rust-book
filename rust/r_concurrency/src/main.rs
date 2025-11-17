df404b17d1b67d9c9bacec713d2d1315c9424d55
calebwelch1 <calebjohn@posteo.net>
2025-11-17 18:27:53 -0500

    concurrency

--- rust/r_concurrency/src/main.rs
+++ rust/r_concurrency/src/main.rs
@@ -49,3 +49,74 @@ let handle = thread::spawn(move || {
     })
 handle.join().unwrap();
 }
+
+// channels
+// analagous to channel of water, send data downstream to other threads
+
+use std::sync::mpsc;
+// multi producer single consumer...
+// multiple producers of messages but one consumer
+use std::thread;
+
+fn main() {
+    mpsc::channel(); // returns tuple of sender and receiver
+    // transmitter, receiver
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let msg = String::from("hi");
+        tx.send(msg).unwrap();
+    })
+
+    let received = rx.recv().unwrap();
+    // also rx.try_recv().unwrap(); will not block main thread execution
+}
+
+// ------------ sharing state
+
+// Only one thread can lock (access) the data inside the mutex at a time.
+
+// Other threads trying to lock it will block until the mutex becomes available.
+
+// Rust wraps mutexes in safe abstractions that guarantee you cannot misuse them without the compiler noticing.
+
+use std::sync::Mutex;
+
+fn main() {
+    let m: Mutex<i32> = Mutex::new(5);
+
+    {
+        // unwrap - if lock fails then panic
+        // when mutexguard goes out of scope it auto releases lock
+        let mut num = m.lock.unwrap();
+        *num = 6;
+    }
+}
+use std::thread;
+use std::rc::Rc;
+use std::synce::{Arc, Mutex};
+
+fn main() {
+    let counter = Arc::new(Mutex::new(0));
+
+    let mut handles = vec![];
+
+    // counter will be owned by each iteration
+    // must allow it to have multiple owners
+    // need something like Rc but thread safe
+    // -> Arc
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num +=1;
+        });
+        handles.push(handle);
+    }
+    for handle in handles{
+        handle.join().unwrap();
+    }
+    
+    println!("Result: {}", *counter.lock().unwrap());
+}
